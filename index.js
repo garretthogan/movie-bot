ce47f2eb1373862a7c05e6726c79e41e2450de06
Garrett Hogan <garrett.hogan@nike.com>
2018-06-14 14:44:24 -0700

    added / endpoint

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ const express = require('expresss');
 const PORT = process.env.PORT || 8080;
 const app = express();
 
+app.get('/', (req, res) => {
+  res.sentStatus(200);
+});
+
 app.get('/slack', (req, res) => {
   res.send('Hello world!');
 });
