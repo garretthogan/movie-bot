50d4d63186182d0631eb29a5493055d69459b0c1
Garrett Hogan <garrett.hogan@nike.com>
2018-06-14 14:57:51 -0700

    get movie score by id

--- index.js
+++ index.js
@@ -1,14 +1,26 @@
 const express = require('express');
+const fetch = require('node-fetch');
+require('dotenv').config();
+
+const baseUrl = 'http://www.omdbapi.com';
 
 const PORT = process.env.PORT || 8080;
 const app = express();
+
+function getMovie(id) {
+  return fetch(`${baseUrl}/?apikey=${apikey}&i=${id}`)
+    .then(data => data.json());
+}
 
 app.get('/', (req, res) => {
   res.sendStatus(200);
 });
 
 app.post('/slack', (req, res) => {
-  res.send('Hello world!');
+  const imdbId = req.text;
+  getMovie(imdbId).then((movie) => {
+    res.send(movie.Metascore);
+  });
 });
 
 app.listen(PORT, () => {
