75f4569be67f11a97629ab68112f96130b8d8221
Garrett Hogan <garrett.hogan@nike.com>
2018-06-14 15:02:59 -0700

    added body parser

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const fetch = require('node-fetch');
+const bodyParser = require('body-parser');
 require('dotenv').config();
 
 const baseUrl = 'http://www.omdbapi.com';
 
 const PORT = process.env.PORT || 8080;
 const app = express();
+app.use(bodyParser.json());
 
 function getMovie(id) {
   return fetch(`${baseUrl}/?apikey=${process.env.OMDB_ACCESS_KEY}&i=${id}`)
@@ -17,7 +19,8 @@ app.get('/', (req, res) => {
 });
 
 app.post('/slack', (req, res) => {
-  const imdbId = req.text;
+  console.log(req.body);
+  const imdbId = req.body.text;
   getMovie(imdbId).then((movie) => {
     res.send(movie.Metascore);
   });
