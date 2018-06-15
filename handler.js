284ec33121415f6418c705f432f4b0610420f5a3
Sean O'Hollaren <Sean.OHollaren2@nike.com>
2018-06-14 20:53:32 -0700

    Add compareMovies lambda

--- handler.js
+++ handler.js
@@ -10,7 +10,7 @@ function getMovie(id) {
 }
 
 function getMovieByTitle(title) {
-  return fetch(`${baseUrl}/?apikey=${apikey}&t=${title}`)
+  return fetch(`${baseUrl}/?apikey=${apikey}&t=${title.trim()}`)
     .then(data => data.json());
 }
 
@@ -36,6 +36,39 @@ function getRating(event, context, callback) {
   });
 }
 
+function compareMovies(event, context, callback) {
+  
+  console.log('Incoming event:', event);
+
+  // grab the title out of the URL encoded body
+  const movies = qs.parse(event.body).text.split(',');
+  
+  console.log(`Extracted titles: ${movies}`);
+  
+  Promise.all([getMovieByTitle(movies[0]), getMovieByTitle(movies[1])])
+  .then((movies) => {
+    console.log(movies);
+
+    // sort movies in descending order
+    const orderedMovies = movies.sort((a, b) => parseInt(b.Metascore) - parseInt(a.Metascore));
+
+
+    const message = {
+      response_type: 'in_channel',
+      text: `*The winner is:* ${orderedMovies[0].Title}  \n\n  *Scores:*\n  ${orderedMovies[0].Title} - ${orderedMovies[0].Metascore} \n  ${orderedMovies[1].Title} - ${orderedMovies[1].Metascore}`,
+    };
+
+    callback(null, {
+      statusCode: 200,
+      body: JSON.stringify(message)
+    });
+  })
+  .catch((err) => console.log('Caught exception', err));
+}
+
+
+
 module.exports = {
-  getRating
+  getRating,
+  compareMovies
 };
